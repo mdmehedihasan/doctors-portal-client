84f9aa532fa9aeb948b36c2e8b3fc27654c1d0f5
mehedi hasan <mehedicsebu@gmail.com>
2022-05-10 16:36:35 +0600

    Browser router in index.js file and Navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-slate-100 text-6xl'>Doctors Portal</h1>
-      <h3 className='bg-red-300'>Hello</h3>
+      <Navbar></Navbar>
+
     </div>
   );
 }

e530fff5ac8640bd4c9a21b1a6e3958739f52bfc
mehedi hasan <mehedicsebu@gmail.com>
2022-05-10 15:56:06 +0600

    tailwind css added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Doctors Portal</h1>
+      <h1 className='bg-slate-100 text-6xl'>Doctors Portal</h1>
+      <h3 className='bg-red-300'>Hello</h3>
     </div>
   );
 }

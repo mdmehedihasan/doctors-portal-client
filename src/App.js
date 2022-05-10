7193adc2c73288674eb94543bf05f5f782ab3536
mehedi hasan <mehedicsebu@gmail.com>
2022-05-10 17:17:42 +0600

    navbar edited and add Link from route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { Routes, Route, useParams } from "react-router-dom";
 import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
 
     </div>
   );

ec96ec4d5fc7c74ef4ecd516ce8de9446bcc9412
mehedi hasan <mehedicsebu@gmail.com>
2022-05-14 10:01:06 +0600

    react tostify intalled and data set for client to server

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -5,6 +5,7 @@ import auth from '../../firebase.init';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const [user, loading, error] = useAuthState(auth);
+    const formattedDate = format(date, 'PP');
 
     const { _id, name, slots } = treatment;
 
@@ -13,7 +14,31 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
         const slot = event.target.slot.value;
 
         console.log(slot, _id, name);
-        setTreatment(null)
+
+        const booking = {
+            treatmentId: _id,
+            treatment: name,
+            date: formattedDate,
+            slot,
+            patient: user.email,
+            patientName: user.displayName,
+            phone: event.target.phone.value
+        }
+
+
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                //to close the model
+                console.log(data);
+                setTreatment(null);
+            })
 
 
 

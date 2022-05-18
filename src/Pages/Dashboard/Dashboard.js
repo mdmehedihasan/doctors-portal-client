b6b99f70f6513ece601700d28573c0ff378c9fdf
mehedi hasan <mehedicsebu@gmail.com>
2022-05-18 07:27:55 +0600

    user data load successfully

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -18,6 +18,7 @@ const Dashboard = () => {
                     <li><Link to="/dashboard">My Appointment</Link></li>
                     <li><Link to="/dashboard/review">My Reviews</Link></li>
                     <li><Link to="/dashboard/history">My History</Link></li>
+                    <li><Link to="/dashboard/users">All Users</Link></li>
 
                 </ul>
 
